7468d5c5f3dfa930ccccab333fc738fef51c7da1
Adrian Roussev <a.roussev@mail.utoronto.ca>
2023-06-13 13:40:25 -0400

    add to carnival class

--- lib/carnival.rb
+++ lib/carnival.rb
@@ -35,5 +35,44 @@ class Carnival
         rides: ride_summary
       }
     end
+    def self.total_revenues(carnivals)
+        carnivals.sum { |carnival| carnival.total_revenue }
+      end
+  
+      def count_visitors
+          rides.flat_map { |ride| ride.rider_log }.map { |rider_log| rider_log.first }.uniq.size
+  
+      end
+  
+      def visitor_summary
+          rides.flat_map do |ride|
+            ride.rider_log.map do |visitor, ride_count|
+              {
+                visitor: visitor,
+                favorite_ride: visitor_favorite_ride(visitor),
+                total_money_spent: total_money_spent(visitor)
+              }
+            end
+          end
+        end
+      
+        def visitor_favorite_ride(visitor)
+          rides.max_by { |ride| ride.rider_log[visitor].to_i }
+        end
+      
+        def total_money_spent(visitor)
+          rides.sum { |ride| ride.rider_log[visitor].to_i * ride.admission_fee }
+        end
+      
+        def ride_summary
+          rides.map do |ride|
+            {
+              ride: ride,
+              riders: ride.rider_log.keys,
+              total_revenue: ride.total_revenue
+            }
+          end
+        end
+      end  
   
     
